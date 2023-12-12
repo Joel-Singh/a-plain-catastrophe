0822c163c456d40b7ed58826969694148498f2bf
Joel Singh <joelsingh788@gmail.com>
2023-12-12 13:16:22 -0500

    Format

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -7,6 +7,5 @@ func _input(event):
   if event is InputEventMouseMotion:
     position = event.position
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
   pass
