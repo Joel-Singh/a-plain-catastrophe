67762d3f11d7d7313c2783bd8c53c102e81d817a
Joel Singh <joelsingh788@gmail.com>
2024-01-15 22:21:20 -0500

    Change Obstacle to RigidBody2D

--- scenes/Obstacle.gd
+++ scenes/Obstacle.gd
@@ -1,10 +1,1 @@
-extends Node2D
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-  const speed: int = 250;
-  goDown(speed, delta)
-
-func goDown(speed, delta):
-  position.y += speed * delta;
+extends RigidBody2D
