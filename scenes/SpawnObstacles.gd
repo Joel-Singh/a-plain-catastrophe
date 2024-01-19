0975c1938ca0b6d0f76a30da55d054a1ca52dd72
Joel Singh <joelsingh788@gmail.com>
2024-01-19 01:28:14 -0500

    Obstacles spawn in main scene

---
+++ scenes/SpawnObstacles.gd
@@ -0,0 +1,19 @@
+extends Node2D
+var obstacle = preload("res://scenes/Obstacle.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+  pass
+
+
+func _on_obstacle_timer_timeout():
+  var newObstacle = obstacle.instantiate()
+  var screen = get_viewport_rect()
+  newObstacle.position.x = randi() % int(screen.size.x)
+  
+  add_child(newObstacle)
