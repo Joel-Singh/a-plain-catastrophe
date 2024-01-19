b49b610603b4beea18d1238d764f6ac44a17f621
Joel Singh <joelsingh788@gmail.com>
2024-01-19 01:27:59 -0500

    Player scales down based on x velocity

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -3,7 +3,10 @@ extends RigidBody2D
 func _integrate_forces(state):
   var mousePosition: Vector2 = get_viewport().get_mouse_position()
   var towardsMousePosition: Vector2 = mousePosition - position
-  var resulting: Vector2 = towardsMousePosition
-  state.linear_velocity = resulting
+  state.linear_velocity = towardsMousePosition
+  var newScale: float = ((300 - abs(state.linear_velocity.x)) / 300) 
+  var children = get_children()
+  for child in children:
+    child.scale.x = max(0.2, newScale)
 func _on_body_entered(body):
   queue_free()
