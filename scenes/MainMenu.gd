2e4150b3d2eb81075b2e812069e54f5930cefaaf
Joel Singh <joelsingh788@gmail.com>
2024-04-15 01:11:11 -0400

    what I came back to

---
+++ scenes/MainMenu.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+  pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+  pass
+
+
+
+var level1 = preload("res://scenes/levels/level1/level1.tscn").instantiate()
+func _on_button_button_down() -> void:
+  get_tree().root.change_scene_to_file(level1)
+  pass # Replace with function body.
